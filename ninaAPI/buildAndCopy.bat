44755d9a565df2341519c0a28d4189423761d8fb
rennmaus-coder <77056626+rennmaus-coder@users.noreply.github.com>
2024-12-28 17:16:38 +0100

    update pipline and api doc

--- ninaAPI/buildAndCopy.bat
+++ ninaAPI/buildAndCopy.bat
@@ -1,14 +1,14 @@
 dotnet build C:\Users\Christian\source\repos\NINA\ninaAPI\ninaAPI\ninaAPI.csproj /property:GenerateFullPaths=true /consoleloggerparameters:NoSummary /p:Configuration=Release /p:Platform="AnyCPU"
-pwsh -Command Compress-Archive -Path 'C:\Users\Christian\AppData\Local\NINA\Plugins\3.0.0\Advanced API\*' -Destination ninaAPI.zip -Force
-set /p beta="Should this be a beta release? y/n: "
-set /p version="Version: "
-if %beta%==y (
-    set /p b_v="What beta version is that?: "
-    gh release create %version%-b.%b_v% ninaAPI.zip -title ninaAPI %version%-beta %b_v% -p
-    pwsh.exe CreateNET7Manifest.ps1 -file ninaAPI.zip -beta -installerUrl "https://github.com/christian-photo/ninaAPI/releases/download/%version%-b.%b_v%/ninaAPI.zip"
-)
-else (
-    gh release create %version% ninaAPI.zip -title %version%
+set /p "beta=Should this be a beta release? y/n: "
+echo "%beta%"
+set /p "version=Version: "
+echo "%version%"
+set /p "b=Beta Version (leave empty for no beta release): "
+if "%beta%" == "y" (
+    pwsh.exe CreateNET7Manifest.ps1 -file "C:\Users\Christian\AppData\Local\NINA\Plugins\3.0.0\Advanced API\ninaAPI.dll" -includeAll -createArchive -beta -installerUrl "https://github.com/christian-photo/ninaAPI/releases/download/%version%-b.%b%/ninaAPI.zip"
+    gh release create "%version%-b.%b%" -t "ninaAPI %version%-beta %b%" -p -R christian-photo/ninaAPI ninaAPI.zip
+) ELSE (
+    gh release create %version% -t "ninaAPI %version%" -R christian-photo/ninaAPI ninaAPI.zip
     pwsh.exe CreateNET7Manifest.ps1 -file ninaAPI.zip -installerUrl "https://github.com/christian-photo/ninaAPI/releases/download/%version%/ninaAPI.zip"
 )
 cd ..
