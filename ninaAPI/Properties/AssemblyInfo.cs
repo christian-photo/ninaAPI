ffa39ffb574ca37ec7ab53738f63d968991c9764
christian-photo <77056626+christian-photo@users.noreply.github.com>
2025-11-15 14:57:17 +0100

    increase version number

--- ninaAPI/Properties/AssemblyInfo.cs
+++ ninaAPI/Properties/AssemblyInfo.cs
@@ -6,8 +6,8 @@ using System.Runtime.InteropServices;
 
 // [MANDATORY] The assembly versioning
 //Should be incremented for each new build of a plugin
-[assembly: AssemblyVersion("2.2.11.1")]
-[assembly: AssemblyFileVersion("2.2.11.1")]
+[assembly: AssemblyVersion("2.2.12.0")]
+[assembly: AssemblyFileVersion("2.2.12.0")]
 
 // [MANDATORY] The name of your plugin
 [assembly: AssemblyTitle("Advanced API")]
